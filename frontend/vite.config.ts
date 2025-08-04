227c46f7d5b80c6739601e98ee5fb953dcd3ab79
Favour Benjamin <favemeka146@gmail.com>
2025-08-04 16:27:31 +0100

    Implemented Live Chat Support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      src: "/src",
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react(),
+      {
+        name: 'html-transform',
+        transformIndexHtml: {
+          enforce: 'pre',
+          transform(html) {
+            return html.replace(
+              /%VITE_SMARTSUPP_KEY%/g,
+              env.VITE_SMARTSUPP_KEY || ''
+            )
+          }
+        }
+      }
+    ],
+    resolve: {
+      alias: {
+        src: "/src",
+      },
     },
-  },
+  }
 })
